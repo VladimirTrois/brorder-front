a8537b6a90d497dbc165724527a37a77c5ba7ff0
vladimir <vladimir.trois@gmail.com>
2024-11-23 15:17:50 +0100

    Nuxt app beginning

--- app.vue
+++ app.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtLoadingIndicator color="secondary" />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+  const config = useAppConfig()
+  useSeoMeta({
+    title: config.title,
+    description: config.description,
+    author: config.author,
+  })
+  useHead({
+    link: config.link,
+    htmlAttrs: { lang: 'fr' },
+  })
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,    
+    },
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss'
+  ],
+  image: {
+    // You can customize the image settings here if needed
+  },
+  fonts: {
+    // Options
+    families: [
+      // only resolve this font with the `google` provider
+      { name: 'Roboto Slab', provider: 'google' },
+    ],
+    defaults: {
+      styles: ['normal'],
+    }
+  }
 })
