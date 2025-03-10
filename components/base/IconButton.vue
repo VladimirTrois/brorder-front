459f846a25513c80181e035b7c3502a2497106b8
vladimir <vladimir.trois@gmail.com>
2025-03-10 17:47:08 +0100

    pagination

--- components/base/IconButton.vue
+++ components/base/IconButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button :class="['buttonWithIcon', 'group', colorClassesButton, sizeClass]">
-    <Icon :class="['icon', colorClassesIcon, sizeClass]" :name="name" />
+    <Icon :class="[colorClassesIcon, sizeClass]" :name="name" />
   </button>
 </template>
 
